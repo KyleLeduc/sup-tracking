140b8cd79047018bc9a2197dc631ac655dd5d77b
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2024-03-23 09:35:53 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- todo add app structure -->
+  <h1>SupTracker</h1>
+  <RouterView />
+</template>
+
+<style scoped></style>
